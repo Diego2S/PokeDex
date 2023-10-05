2b4b939a27fa6dc27ee9b947c2149785e99844ce
Diego2S <limao.smart@gmail.com>
2023-10-05 18:26:42 -0300

    adicionando elementos <li> dinamicamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,41 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonLi(pokemon){
+    return`
+    <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+
+                    <img src="https://archives.bulbagarden.net/media/upload/2/24/001Bulbasaur_Dream_4.png" 
+                    alt=${pokemon.name}>
+                </div>
+                
+                
+            </li>
+    `
+}
+
+const pokemonList= document.getElementById('pokemonList')
 
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length;i++){
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonLi(pokemon)
+            
+        }}
+        )
     .catch((error) => console.log(error))
-    .finally(() => console.log('Requisição concluida'))
+   
 
 
 
