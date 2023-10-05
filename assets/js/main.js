50196a36f9a6c0d806ea347740ce7709bf1a2819
Diego2S <limao.smart@gmail.com>
2023-10-04 22:06:58 -0300

    exemplo de arrow function

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,10 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 
 fetch(url)
-    .then(function(response){
-    return response.json()})
-    .then(function (jsonBody){    
-    console.log(jsonBody)})
-    .catch(function(error){
-    console.log(error)})
-    .finally(function(){
-    console.log('Requisição concluida')
-})
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
+    .finally(() => console.log('Requisição concluida'))
 
 
 
